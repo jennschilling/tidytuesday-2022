680b088bb5f4a11b75480c669b00e1ac082cf7b2
jaschilling <jaschilling@arizona.edu>
2022-01-21 10:01:02 -0700

    working on grid

--- 2022-01-18/2022-01-18.R
+++ 2022-01-18/2022-01-18.R
@@ -15,33 +15,71 @@ library(countrycode)
 
 chocolate <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-18/chocolate.csv')
 
+
+#### Process the Data ####
+
 choc_dat <- chocolate %>%
   mutate(cocoa_percent = parse_number(cocoa_percent),
+         num_ingredients = parse_number(ingredients),
          country_of_bean_origin = str_replace_all(country_of_bean_origin, "[.]", ""),
-         company_location = str_replace_all(company_location, "[.]", ""))
+         country_of_bean_origin = case_when(
+           country_of_bean_origin == "Burma" ~ "Myanmar (Burma)", 
+           country_of_bean_origin == "Congo" | country_of_bean_origin == "DR Congo" ~ "Congo - Kinshasa",
+           country_of_bean_origin == "Ivory Coast" ~ "Côte d’Ivoire",
+           country_of_bean_origin == "Principe" | country_of_bean_origin == "Sao Tome" | 
+             country_of_bean_origin == "Sao Tome & Principe" ~ "São Tomé & Príncipe",
+           country_of_bean_origin == "St Lucia" ~ "St. Lucia",
+           country_of_bean_origin == "StVincent-Grenadines" ~ "St. Vincent & Grenadines",
+           country_of_bean_origin == "Trinidad" | country_of_bean_origin == "Tobago" ~ "Trinidad & Tobago",
+           country_of_bean_origin == "USA" ~ "United States",
+           TRUE ~ country_of_bean_origin),
+         company_location = str_replace_all(company_location, "[.]", ""),
+         company_location = case_when(
+           company_location == "Burma" ~ "Myanmar (Burma)", 
+           company_location == "Congo" | company_location == "DR Congo" ~ "Congo - Kinshasa",
+           company_location == "Ivory Coast" ~ "Côte d’Ivoire",
+           company_location == "Principe" | company_location == "Sao Tome" | 
+             company_location == "Sao Tome & Principe" ~ "São Tomé & Príncipe",
+           company_location == "St Lucia" ~ "St. Lucia",
+           company_location == "StVincent-Grenadines" ~ "St. Vincent & Grenadines",
+           company_location == "Trinidad" | company_location == "Tobago" ~ "Trinidad & Tobago",
+           company_location == "USA" ~ "United States",
+           company_location == "UK" | company_location == "Scotland" |
+             company_location == "Wales" ~ "United Kingdom",
+           company_location == "UAE" ~ "United Arab Emirates",
+           company_location == "Amsterdam" ~ "Netherlands",
+           company_location == "Czech Republic" ~ "Czechia",
+           TRUE ~ company_location)) %>%
+  left_join(codelist %>% select(country.name.en, continent, region), by = c("country_of_bean_origin" = "country.name.en")) %>%
+  rename(cont_of_bean_origin = continent,
+         reg_of_bean_origin = region) %>%
+  left_join(codelist %>% select(country.name.en, continent, region), by = c("company_location" = "country.name.en")) %>%
+  rename(cont_company_location = continent,
+         reg_company_location = region) 
 
-# Cocoa percents with the most chocolates
-# 70 - 1046
-# 75 - 310
-# 72 - 295
-# 65 - 90
-# 80 - 89
-# 68 - 72
-# 74 - 67
-# 73 - 66
 
-# 100% - 21
-# Less than 60% 8 + 1 + 2+ 16 + 1 + 1 + 1 + 1 = 31
-# More than 90% 3 + 2 + 21 = 26
+## Plan ##
+# 1. Sample 20 chocolates - removing NAs from continent or region of company/bean location, number of ingredients, cocoa percent, rating
+# 2. Arrange chocolates from highest rated to lowest rated in a grid
+# 3. Fill shape by cocoa percent
+# 4. Type of shape by continent of bean location (number of sides)
+# 5. Color of decoration by continent of company 
+# 6. Number of decorations by number of ingredients
 
-# Ingredients with the most chocolates
-# 3- B,S,C - 999
-# 2- B,S - 718
-# 4- B,S,C,L - 286
-# 5- B,S,C,V,L - 184
-# 4- B,S,C,V - 141
+choc_dat_sample <- choc_dat %>%
+  select(cont_company_location, cont_of_bean_origin, num_ingredients, cocoa_percent, rating) %>%
+  filter(complete.cases(.))
 
-# Uses alternate sugar: 31 + 12 + 1 + 2 + 20 + 7 + 3 = 76
+# Step 1 - Sample
+get_choc_sample <- slice_sample(choc_dat_sample, n = 20)
+
+# Step 2 - Create Grid
+choc_grid <- get_choc_sample %>%
+  arrange(-rating) %>%
+  mutate(radius = 1)
+
+
+
 
 #### Formatting ####
 
@@ -57,6 +95,14 @@ choc_colors <- c(
   "#593325",
   "#401E12",
   "#260F07" # dark brown
+)
+
+dec_colors <- c(
+  "#FCBDBD", # light pink - Africa
+  "#F66293", # dark pink - Americas
+  "#A1A2E6", # purple - Asia
+  "#91E7EB", # blue - Europe
+  "#B1FDBD" # green - Oceania
 )
 
 gold <- "#D9AE30"
